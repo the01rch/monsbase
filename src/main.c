85f588a3047f64694eb0a829d4d18d6ebd36a863
the01rch <reda.rouich@epitech.eu>
2026-07-22 01:44:38 +0200

    [ADD] field_check

--- src/main.c
+++ src/main.c
@@ -1,32 +1,64 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <cjson/cJSON.h>
 
-int main() {
+int field_check(const char *field, cJSON *mon)
+{
+	if (strcmp(field, "name") == 0)
+		return (printf("%s\n", cJSON_GetObjectItem(mon, field)->valuestring), 0);
+	if (strcmp(field, "id") == 0 || strcmp(field, "bst") == 0) {
+		printf("%lf\n", cJSON_GetObjectItem(mon, field)->valuedouble);
+		return (0);
+	}
+	if (strcmp(field, "types") == 0 || strcmp(field, "abilities") == 0)	{
+		cJSON *arr = cJSON_GetObjectItem(mon, field);
+		for (int i = 0; i < cJSON_GetArraySize(arr); i++) {
+			cJSON *tmp = cJSON_GetArrayItem(arr, i);
+			printf("%s\n", tmp->valuestring);
+		}
+		return (0);
+	}
+	printf("wrong field\n");
+	return (1);
+}
+
+int main(int ac, const char **av) {
     /* Read whole file */
-    FILE *f = fopen("data/mons_OU.json", "r");
+    FILE *f;
+	long len;
+	char *data;
+
+	(void)ac;
+	if (!av[1])
+		return (1);
+	f = fopen("data/mons_OU.json", "r");
+	if (!f)
+		return (1);
+
     fseek(f, 0, SEEK_END);
-    long len = ftell(f);
+    len = ftell(f);
     fseek(f, 0, SEEK_SET);
-    char *data = malloc(len + 1);
+    data = malloc(len + 1);
+	if (!data)
+		return (fclose(f), 1);
     fread(data, 1, len, f);
     fclose(f);
     data[len] = '\0';
 
     /* Parse JSON */
     cJSON *root = cJSON_Parse(data);
-    cJSON *arr = cJSON_GetObjectItem(
-                    cJSON_GetObjectItem(
-                        cJSON_GetObjectItem(root, "tiers"), "OU"),
-                    "pokemon");
+	free(data);
+	if (!root)
+		return (1);
+    cJSON *arr = cJSON_GetObjectItem(root, "pokemon");
 
-    /* Print names */
+    /* Print infos */
     for (int i = 0; i < cJSON_GetArraySize(arr); i++) {
         cJSON *mon = cJSON_GetArrayItem(arr, i);
-        printf("%s\n", cJSON_GetObjectItem(mon, "name")->valuestring);
+		if (field_check(av[1], mon) == 1)
+			return (1);
     }
-
     cJSON_Delete(root);
-    free(data);
-    return 0;
+    return (0);
 }
